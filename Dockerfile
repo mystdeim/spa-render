9cc0aff1b0ae696041c72c4b5191a71742af10e9
Roman Novikov <mystdeim@gmail.com>
2024-09-15 00:12:18 +0100

    migrate to custom playwright

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,45 @@ FROM node:22-slim
 
 WORKDIR /app
 
-RUN apt-get update && \
-    apt-get install -y --no-install-recommends chromium && \
-    rm -rf /var/lib/apt/lists/*
+# Install necessary dependencies for Chromium
+RUN apt-get update && apt-get install -y \
+    libglib2.0-0 \
+    libnss3 \
+    libnspr4 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libdrm2 \
+    libxkbcommon0 \
+    libdbus-1-3 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libgbm1 \
+    libpango-1.0-0 \
+    libcairo2 \
+    libasound2 \
+    libx11-xcb1 \
+    libxext6 \
+    libxfixes3 \
+    libexpat1 \
+    libxcb1 \
+    libx11-6 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libpango-1.0-0 \
+    libcairo2 \
+    libasound2 \
+    && rm -rf /var/lib/apt/lists/*
 
-COPY plugins plugins/
 COPY server.js package.json package-lock.json ./
 
 RUN npm ci
+RUN npx playwright install chromium
 
 EXPOSE 3000
 
--- server.js
+++ server.js
@@ -1,11 +1,96 @@
-const prerender = require('prerender');
+const express = require('express');
+const { chromium } = require('playwright');
+const bodyParser = require('body-parser');
 
-const server = prerender({
-  chromeLocation: '/usr/bin/chromium',
-  logRequests: true,
-  captureConsoleLog: true,
-  chromeFlags: ['--disable-web-security', '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage']
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const BLOCKED_RESOURCES = [
+  "google-analytics.com",
+  "api.mixpanel.com",
+  "fonts.googleapis.com",
+  "stats.g.doubleclick.net",
+  "mc.yandex.ru",
+  "use.typekit.net",
+  "beacon.tapfiliate.com",
+  "js-agent.newrelic.com",
+  "api.segment.io",
+  "woopra.com",
+  "static.olark.com",
+  "static.getclicky.com",
+  "fast.fonts.com",
+  "youtube.com/embed",
+  "cdn.heapanalytics.com",
+  "googleads.g.doubleclick.net",
+  "pagead2.googlesyndication.com",
+  "fullstory.com/rec",
+  "navilytics.com/nls_ajax.php",
+  "log.optimizely.com/event",
+  "hn.inspectlet.com",
+  "tpc.googlesyndication.com",
+  "partner.googleadservices.com",
+  "mail.ru",
+  "calltouch.ru",
+  "facebook.net",
+  "vk.com",
+  "chatra.io",
+  "googletagmanager.com",
+  "bitrix24.ru"
+];
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+let browser;
+
+const isBlocked = (url) => {
+  return BLOCKED_RESOURCES.some(substring => url.includes(substring));
+}
+
+app.post('/render', async (req, res) => {
+  const { url } = req.body;
+  if (!url) {
+    return res.status(400).send('URL parameter is required');
+  }
+
+  try {
+    const page = await browser.newPage();
+    await page.route("**/*", (route, request) => {
+      const url = request.url()
+      const resourceType = request.resourceType();
+      const blockedTypes = ["image", "stylesheet", "media", "xhr", "fetch", "font", "manifest", "websocket", "x-icon", "ping", "preflight"];
+
+      if (blockedTypes.includes(resourceType) || isBlocked(url)) {
+        console.log(`Blocked: ${url}`);
+        route.abort();
+      } else {
+        console.log(`Downloading: ${url}`);
+        route.continue();
+      }
+    });
+    await page.goto(url, { waitUntil: 'networkidle' });
+    const content = await page.content();
+    await page.close();
+
+    res.send(content);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while rendering the page');
+  }
 });
-server.use(require('./plugins/blockResources'));
-server.use(require('./plugins/removeScriptTags'));
-server.start();
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  browser = await chromium.launch({
+    headless: true
+  });
+  console.log('Browser launched');
+});
+
+process.on('SIGINT', async () => {
+  if (browser) {
+    await browser.close();
+    console.log('Browser closed');
+  }
+  process.exit();
+});
